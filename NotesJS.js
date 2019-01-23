550e81bc71aa8422e8fda5c9a0007d748d9b7155
JaffarShah <sjeffshah@gmail.com>
2019-01-23 21:56:49 +0000

    Learn JavaScript

---
+++ NotesJS.js
@@ -0,0 +1,38 @@
+
+// double slash: in-line comment
+
+
+/* slash and star makes it possible to comment in JS over mutiple lines (multi-line comment). When closing this option, star comes first and slash follows.
+
+Data Types:
+undefined, null, boolean, string, symbol, number, and object
+
+
+
+Three ways to to declare a variable in JS: var, let and const
+
+about const: you can never change it.
+
+Capitalization in JS matters.
+
+Adding two numbers in JS is pretty straightfoward.
+
+*/
+
+var cityName = "Vancouver"
+cityName = 8
+
+let ourName = "BC"
+
+const pi = 3.14
+
+var a; // declaring a varible, with ;
+var b = 2; // declaring and assigning a varible in one line by = and ;
+// i.e 2 is assigned to be 'b'.
+
+
+console.log(a) //prints out result of what is between ()
+
+var a = 1;
+console.log(a) //result should be 1.
+
